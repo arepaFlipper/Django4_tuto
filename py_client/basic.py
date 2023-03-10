24fd2d41eb9a409c7197db7e71b3a6546fc11810
cris <ctovar@abexuspr.com>
2023-03-10 18:40:53 -0500

    ☘️ (0:17:50)test httpbin.org endpoint
    run python ./py_client/basic.py to test it out

---
+++ py_client/basic.py
@@ -0,0 +1,7 @@
+import requests
+
+endpoint = "https://httpbin.org/status/200/"
+endpoint = "https://httpbin.org/"
+
+get_response = requests.get(endpoint) 
+print("""🔏   \x1b[1;34;40m get_response:"""+get_response.text+"\x1b[0m") ## DELETEME
