5b7467550bbb7f6c48044a65815ed4129faf79e6
cris <ctovar@abexuspr.com>
2023-03-11 17:46:40 -0500

    🍔

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from products.serializers import ProductSerializer
 
 @api_view(["POST"])
 def api_home(request, *args,**kwargs):
     """
     DRF API View
     """
-    data = request.data
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        print(serializer.data)
+        data = serializer.data
+        return Response(data)
