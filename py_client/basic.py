4465775e5ed95d3e1dcb82d6d555fa96b72a0a8f
cris <ctovar@abexuspr.com>
2023-03-10 19:22:41 -0500

    🌫️ (00:25:52)fix the latest error due to not existing url

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,12 @@
 import requests
 
 endpoint = "https://httpbin.org/status/200/"
-# endpoint = "https://httpbin.org/anything"
+endpoint = "https://httpbin.org/anything"
 
 get_response = requests.get(endpoint, json={"query":"Hello world"}) 
 print("""🔏\x1b[1;34;40mget_response in regular text:"""+get_response.text+"\x1b[0m") ## DELETEME
 
-print("""🚋\x1b[1;37;41mget_response.status_code:""") ## DELETEME
+print("""📗 \x1b[1;32;40mget_response.status_code:""") ## DELETEME
 print(get_response.status_code) ## DELETEME
 print('\x1b[0m') ## DELETEME
+
