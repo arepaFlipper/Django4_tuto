e565e6fd0d066d6c322db27ed0d0ae806682776d
cris <ctovar@abexuspr.com>
2023-03-10 19:21:05 -0500

    👏 (00:25:41) test the status_code

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,11 @@
 import requests
 
 endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint, data={"query":"Hello world"}) 
+get_response = requests.get(endpoint, json={"query":"Hello world"}) 
 print("""🔏\x1b[1;34;40mget_response in regular text:"""+get_response.text+"\x1b[0m") ## DELETEME
-print("""⛺\x1b[1;36;40mget_response in JSON:""") ## DELETEME
-print(get_response.json()) ## DELETEME
+
+print("""🚋\x1b[1;37;41mget_response.status_code:""") ## DELETEME
+print(get_response.status_code) ## DELETEME
 print('\x1b[0m') ## DELETEME
