7c4a16f22af77d264d19124eb518c77df4fca9a1
cris <ctovar@abexuspr.com>
2023-03-11 12:48:03 -0500

    🌔 (1:04:08) comeback to the same view where we just use rest_framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,13 @@
 from django.forms.models import model_to_dict
-import json
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from products.models import Product
 
-@api_view(["GET","POST"])
+@api_view(["GET"])
 def api_home(request, *args,**kwargs):
     """
     DRF API View
     """
-    if request.method != "POST":
-        return Response({"detail":"GET not allowed"}, status=405)
-        
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
