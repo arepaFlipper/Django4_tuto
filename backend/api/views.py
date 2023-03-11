75d73b7acf80af9eb779ddab4c25f6ba8fafb687
cris <ctovar@abexuspr.com>
2023-03-11 07:10:30 -0500

    ↩️  not getting the body

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,8 @@
 from django.http import JsonResponse
 
-def api_home(request,*args,**kwargs):
+def api_home(request, *args,**kwargs):
+    body = request.body
+    print("""😲   \x1b[1;33;40mviews.py:6   body:""") ## DELETEME
+    print(body) ## DELETEME
+    print('\x1b[0m') ## DELETEME
     return JsonResponse({'message': "Hi there, this is your Django API response!!"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,11 +4,14 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:7890/api"
 
-get_response = requests.get(endpoint, json={"query":"Hello world"}) 
+print("""🖼️   \x1b[1;37;40mbasic.py:7   requests.__version__:""") ## DELETEME
+print(requests.__version__) ## DELETEME
+print('\x1b[0m') ## DELETEME
+get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello world"}) 
 print("""🔏\x1b[1;34;40mget_response in regular text:"""+get_response.text+"\x1b[0m") ## DELETEME
 
 print("""⛺\x1b[1;36;40mget_response in JSON:""") ## DELETEME
-print(get_response.json()['message']) ## DELETEME
+print(get_response.json()) ## DELETEME
 print('\x1b[0m') ## DELETEME
 
 print("""📗 \x1b[1;32;40mget_response.status_code:""") ## DELETEME
