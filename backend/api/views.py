80acfef4f2674edbd1dd2bb9ad97e326a3cca4bd
cris <ctovar@abexuspr.com>
2023-03-11 17:35:29 -0500

    📄 (1:17:50)comeback the view to some preview version:
    a version where it return very same data given, but the extra protection
    of the rest_framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,10 @@
-from django.forms.models import model_to_dict
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from products.models import Product
-from products.serializers import ProductSerializer
+from rest_framework.response import Response
 
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args,**kwargs):
     """
     DRF API View
     """
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        data = ProductSerializer(instance).data
+    data = request.data
     return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:7890/api/?this_arg=this_value"
 
-get_response = requests.get(endpoint, params={"product_id":123}, json={"query":"Hello world"}) 
+get_response = requests.post(endpoint, params={"title":"hello world"}, json={"query":"Hello world"}) 
 print("""🔏\x1b[1;34;40mget_response headers:""") ## DELETEME
 print(get_response.headers)
 # print("""🔏\x1b[1;33;40mget_response in regular text: """+get_response.text+"\x1b[0m") ## DELETEME
