7638e6d0fbfc36189a9383eb447b4346412882a2
cris <ctovar@abexuspr.com>
2023-03-11 18:05:24 -0500

    🍆 (1:19:06)set the serializer to validate the data given

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,7 +7,16 @@ def api_home(request, *args,**kwargs):
     """
     DRF API View
     """
+    print("""💬   \x1b[1;34;40mviews.py:10  request:""") ## DELETEME
+    print(request) ## DELETEME
+    print('\x1b[0m') ## DELETEME
     serializer = ProductSerializer(data=request.data)
+    print("""💿   \x1b[1;37;45mviews.py:11  serializer.is_valid():""") ## DELETEME
+    print(serializer.is_valid()) ## DELETEME
+    print('\x1b[0m') ## DELETEME
+    print("""📳   \x1b[1;33;40mviews.py:20  request.data:""") ## DELETEME
+    print(request.data) ## DELETEME
+    print('\x1b[0m') ## DELETEME
     if serializer.is_valid():
         print(serializer.data)
         data = serializer.data
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,14 +4,14 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:7890/api/?this_arg=this_value"
 
-get_response = requests.post(endpoint, params={"title":"hello world"}, json={"query":"Hello world"}) 
+get_response = requests.post(endpoint, params={"product_id":123}, json={"title":"Hello world"}) 
 print("""🔏\x1b[1;34;40mget_response headers:""") ## DELETEME
 print(get_response.headers)
 # print("""🔏\x1b[1;33;40mget_response in regular text: """+get_response.text+"\x1b[0m") ## DELETEME
 
-# print("""⛺\x1b[1;36;40mget_response in JSON:""") ## DELETEME
-# print(get_response.json()) ## DELETEME
-# print('\x1b[0m') ## DELETEME
+print("""⛺\x1b[1;36;40mget_response in JSON:""") ## DELETEME
+print(get_response.json()) ## DELETEME
+print('\x1b[0m') ## DELETEME
 
 print("""📗 \x1b[1;32;40mget_response.status_code:""") ## DELETEME
 print(get_response.status_code) ## DELETEME
