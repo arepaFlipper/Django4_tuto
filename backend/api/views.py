9b5b040fb1754201202148cf41a974c3b931fb26
cris <ctovar@abexuspr.com>
2023-03-11 12:43:51 -0500

    🛂 (1:03:21) replicate the same respond for a GET request

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,19 @@
 from django.forms.models import model_to_dict
 import json
 from rest_framework.response import Response
-from django.http import JsonResponse, HttpResponse
 from rest_framework.decorators import api_view
 from products.models import Product
 
-@api_view(["POST"])
+@api_view(["GET","POST"])
 def api_home(request, *args,**kwargs):
     """
     DRF API View
     """
+    if request.method != "POST":
+        return Response({"detail":"GET not allowed"}, status=405)
+        
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id','title','price'])
-    return JsonResponse(data)
+    return Response(data)
