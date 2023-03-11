30f2d083f8fae2f0ab18d367b3bc5b2c17158ea6
cris <ctovar@abexuspr.com>
2023-03-11 05:46:50 -0500

    🔢 (00:36:20) read the message property from the new endpoint

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,10 +2,14 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:7890/"
+endpoint = "http://localhost:7890/api"
 
 get_response = requests.get(endpoint, json={"query":"Hello world"}) 
 print("""🔏\x1b[1;34;40mget_response in regular text:"""+get_response.text+"\x1b[0m") ## DELETEME
+
+print("""⛺\x1b[1;36;40mget_response in JSON:""") ## DELETEME
+print(get_response.json()['message']) ## DELETEME
+print('\x1b[0m') ## DELETEME
 
 print("""📗 \x1b[1;32;40mget_response.status_code:""") ## DELETEME
 print(get_response.status_code) ## DELETEME
