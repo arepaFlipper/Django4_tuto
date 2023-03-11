e55a3c908acfbe1f7fb14a4d06deff9bd6ce78b4
cris <ctovar@abexuspr.com>
2023-03-11 17:12:24 -0500

    🎐 comeback with the basic serializer displaying:
    title, content, price, sale_price, my_disconunt

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,22 +1,7 @@
 from rest_framework import serializers
 from .models import Product
 
-class PrimaryProductSerializer(serializers.ModelSerializer):
-    my_discount = serializers.SerializerMethodField(read_only=True)
-    class Meta:
-        model = Product
-        fields =[
-            'title',
-            'content',
-            'price',
-            'sale_price',
-            'my_discount',
-        ]
-
-    def get_my_discount(self,obj):
-        return obj.get_discount()
-
-class SecondaryProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
