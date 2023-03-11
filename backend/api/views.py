980bd55f1deb18a026a20f7bac82d0412958d958
cris <ctovar@abexuspr.com>
2023-03-11 10:07:17 -0500

    🚑 (00:53:19) update api_home endpoint to return:
    the id, title, contet and price values of a random object

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,29 +1,13 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 def api_home(request, *args,**kwargs):
-    body = request.body
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data= json.loads(body)
-    except Exception as e:
-        pass
-    print("""😲   \x1b[1;33;40mviews.py:6   body:""") ## DELETEME
-    print(body) ## DELETEME
-    print('\x1b[0m') ## DELETEME
-    print("""🚮   \x1b[1;35;40mviews.py:16  data:""") ## DELETEME
-    print(data) ## DELETEME
-    print('\x1b[0m') ## DELETEME
-    print("""🇿🇦   \x1b[1;37;42mviews.py:18  dict(request.GET):""") ## DELETEME
-    print(dict(request.GET)) ## DELETEME
-    print('\x1b[0m') ## DELETEME
-    print("""🐸   \x1b[1;31;40mviews.py:20  request.GET:""") ## DELETEME
-    print(type(request.GET)) ## DELETEME
-    print('\x1b[0m') ## DELETEME
-    data['headers'] = dict(request.headers)
-    data['params'] = dict(request.GET)
-    data['content_type'] = request.content_type
-    print("""🐠   \x1b[1;37;40mviews.py:22  data:""") ## DELETEME
-    print(data) ## DELETEME
-    print('\x1b[0m') ## DELETEME
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
