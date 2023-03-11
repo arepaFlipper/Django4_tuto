9a5f832a7331249f8b9f67ff4505957a00e1e1d6
cris <ctovar@abexuspr.com>
2023-03-11 13:07:36 -0500

    ♓ (1:05:35) define a method on Product model:
    By using the decorator @property in the models.py we may define a method
    on a Django model that behaves like a read-only attribute. This means
    that you can access the method like a regular attribute, without having
    to call it explicitly as a method, i.e. we do not use the "()" at the
    end of the call

--- py_client/shell_commands.py
+++ py_client/shell_commands.py
@@ -1,10 +1,8 @@
 from products.models import Product
 
-Product.objects.create(title="Hello world", content="this is amazing!", price=0.00)
-Product.objects.create(title="Hello world again", content="this is amazing!", price=12.00)
-# get random products
-Product.objects.all().order_by("?").first()
-Product.objects.all().order_by("?").first()
-Product.objects.all().order_by("?").first()
-Product.objects.all().order_by("?").first()
-Product.objects.all().order_by("?").first()
+prod_obj = Product.objects.first()
+prod_obj.sale_price
+prod_obj.sale_price
+prod_obj = Product.objects.last()
+prod_obj.sale_price
+prod_obj.sale_price
