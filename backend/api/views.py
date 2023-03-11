1fb00b9dd509f33fb2e1705b65ad6740d738c06c
cris <ctovar@abexuspr.com>
2023-03-11 10:31:49 -0500

    🕰️ (00:57:00) display the headers ,but:
    the data dict is not as I expected.
    
    JsonResponse accepts a dictionary as a first argument
    HttpResponse accepts an string as a first argument

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from products.models import Product
 
 def api_home(request, *args,**kwargs):
@@ -8,4 +8,4 @@ def api_home(request, *args,**kwargs):
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id','title'])
-    return JsonResponse(data)
+    return HttpResponse(data)
