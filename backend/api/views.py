4afa524f109dfd0d24371e9c584b47586c2ce60c
cris <ctovar@abexuspr.com>
2023-03-11 12:35:43 -0500

    🪠 test the api_view decorator for a view that returns a JsonResponse:
    the view funcs that use rest_framework.response.Response are protected
    from Cross-Site Request Forgery attacks. However, you can protect any
    view func from this CORS attacks by wrap them into api_view decorator,
    this way you must ensure the methods it should respond to, as well.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,11 @@
 from django.forms.models import model_to_dict
 import json
 from rest_framework.response import Response
+from django.http import JsonResponse, HttpResponse
 from rest_framework.decorators import api_view
 from products.models import Product
 
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args,**kwargs):
     """
     DRF API View
@@ -13,4 +14,4 @@ def api_home(request, *args,**kwargs):
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id','title','price'])
-    return Response(data)
+    return JsonResponse(data)
