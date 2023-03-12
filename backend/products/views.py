9878c31390a6501236ae03cca1b0130fef4ce31b
cris <ctovar@abexuspr.com>
2023-03-12 09:54:33 -0500

    ♋ (1:42:21) add a view ProductListAPIView but:
    as we can see the view ProductListAPIView is the very same
    ProductCreateView with the only difference that it inherits from
    ListAPIView so we can actually re-use the ProductCreateView for this
    purpose, due to this the rest_framework.generics was thought to do so in
    order to avoid write repeated code.
    
    Today I also learnt that I can save the document directly in INSERT_MODE
    pressing CTRL + s 🐤 💛 📗 🆑
    
    yesterday I learnt that I can delete from the cursor forwards through
    the line until the end with shift + c

--- backend/products/views.py
+++ backend/products/views.py
@@ -23,3 +23,9 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_list_view = ProductListAPIView.as_view()
