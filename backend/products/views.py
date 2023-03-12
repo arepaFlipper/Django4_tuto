01a37e329ecf59b084ac446aff5927466bc8a409
cris <ctovar@abexuspr.com>
2023-03-12 18:04:16 -0500

    🍫 I forgot to return the ListCreateAPIView attrs to:
    ProductListCreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,7 +8,7 @@ from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
-class ProductListCreateAPIView(CreateAPIView):
+class ProductListCreateAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
