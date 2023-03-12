3532ede7402a8e8ffd68540b7ce2fa9ac93448f7
cris <ctovar@abexuspr.com>
2023-03-12 12:33:59 -0500

    🧰 (1:49:15) add product_alt_view:
    this view handles 3 tasks:
    1. if the request method is GET, and there is a primary key included, it
       displays the details of the product which id match with the primary
       key given.
    
    2. if the request method is GET, and there NO is a primary key included, it diplays a list of all products
    
    3. if the request method is POST, it creates a new product with data
       given by the body

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from django.http import Http404
+from rest_framework.decorators import api_view
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -24,3 +27,24 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 
 product_detail_view = ProductDetailAPIView.as_view()
 
+@api_view(['GET','POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            queryset = Product.objects.filter(pk=pk)
+            if not queryset.exist():
+                raise Http404
+            return Response()
+
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            print (serializer.data)
+            return Response(serializer.data)
+        return Response({"invalid":"not good data"}, status=400)
