b2d8381cdc961671dd562bec838913565c37016e
cris <ctovar@abexuspr.com>
2023-03-12 16:06:46 -0500

    🍯 views refactor

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,11 +1,12 @@
-from rest_framework import generics, mixins
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView,UpdateAPIView, DestroyAPIView, GenericAPIView
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
-class ProductListCreateAPIView(generics.ListCreateAPIView):
+class ProductListCreateAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -21,13 +22,13 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
+class ProductDetailAPIView(RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
 
-class ProductUpdateAPIView(generics.UpdateAPIView):
+class ProductUpdateAPIView(UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "pk"
@@ -39,7 +40,7 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
 
 product_update_view = ProductUpdateAPIView.as_view()
 
-class ProductDeleteAPIView(generics.DestroyAPIView):
+class ProductDeleteAPIView(DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "pk"
@@ -49,11 +50,11 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
 
 product_delete_view = ProductDeleteAPIView.as_view()
 
-class ProductMixinView(mixins.ListModelMixin,generics.GenericAPIView):
+class ProductMixinView(ListModelMixin ,GenericAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    def post(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
 product_mixin_view = ProductMixinView.as_view()
