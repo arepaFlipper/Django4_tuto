2ca1c2025e26898222c70c2e0dd47124af037c47
cris <ctovar@abexuspr.com>
2023-03-12 09:18:33 -0500

    🎬 (1:38:56) create a product despite the field is a type number:
    this is something that I gladly amaze of serializers that with a few
    lines of codes it validates and create the proper data.
    Even if the data given is an string of numbers (like "26.99") works!
    when the data given to property price is something else like "sdf" it
    returns a warning

--- py_client/create.py
+++ py_client/create.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = "http://localhost:7890/api/products/"
 
-data = { "title":"This field is done"}
+data = { "title":"This field is done", "price": 32.99 }
 get_response = requests.post(endpoint, json=data)
 print("""📗 \x1b[1;32;40mget_response.status_code:""") ## DELETEME
 print(get_response.status_code) ## DELETEME
