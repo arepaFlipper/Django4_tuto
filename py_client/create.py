d46e4a92f897c8e192965ffac68660b52fdd1163
cris <ctovar@abexuspr.com>
2023-03-13 07:24:32 -0500

    🔢 (2:54:54) implement get_token into create test endpoint

--- py_client/create.py
+++ py_client/create.py
@@ -1,9 +1,12 @@
 import requests
+from auth_token import get_token
 
 endpoint = "http://localhost:7890/api/products/"
+token = get_token()
+headers = {'Authorization': f'Bearer {token}'}
 
 data = { "title":"This field is done", "price": "32.99"}
-get_response = requests.post (endpoint, json=data)
+get_response = requests.post (endpoint, json=data, headers=headers)
 print("status_code: "+str(get_response.status_code)) ## DELETEME
 print("""📗 \x1b[1;32;40mget_response.status_code:""") ## DELETEME
 print(get_response.json())
