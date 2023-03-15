008e529280068f65df10faa11a57113022f261e7
cris <ctovar@abexuspr.com>
2023-03-15 15:05:11 -0500

    👍 (3:50:13) invalidate specific words

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers 
 from rest_framework.reverse import reverse
 from .models import Product
+from .validators import validate_title_no_hello
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field='pk')
+    title = serializers.CharField(validators=[validate_title_no_hello])
     class Meta:
         model = Product
         fields =[
@@ -21,11 +23,10 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def validate_title(self,value):
         request = self.context.get('request')
-        user = request.user
         print("""📅   \x1b[1;30;43mserializers.py:23    value:""") ## DELETEME
         print(value) ## DELETEME
         print('\x1b[0m') ## DELETEME
-        qs = Product.objects.filter(user=user,title__iexact=value)
+        qs = Product.objects.filter(title__iexact=value)
         print("""📺   \x1b[1;33;40mserializers.py:24    qs:""") ## DELETEME
         print(qs) ## DELETEME
         print('\x1b[0m') ## DELETEME
---
+++ backend/products/validators.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Product
+
+def validate_title(value):
+    print("""📅   \x1b[1;30;43mserializers.py:23    value:""") ## DELETEME
+    print(value) ## DELETEME
+    print('\x1b[0m') ## DELETEME
+    qs = Product.objects.filter(title__iexact=value)
+    print("""📺   \x1b[1;33;40mserializers.py:24    qs:""") ## DELETEME
+    print(qs) ## DELETEME
+    print('\x1b[0m') ## DELETEME
+    if qs.exists():
+        raise serializers.ValidationError(f'"{value}" is already a product name.')
+    return value
+
+def validate_title_no_hello(value):
+    forbidden_title = "hello"
+    if forbidden_title in value.lower():
+        raise serializers.ValidationError(f"'{forbidden_title}' is not allowed")
+    return value
