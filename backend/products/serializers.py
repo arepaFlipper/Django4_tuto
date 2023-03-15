7166a1db5d4705ae158b1db5fff57a37199a98a6
cris <ctovar@abexuspr.com>
2023-03-14 19:02:34 -0500

    🌬️ (3:26:34) add and define url to the serializer:
    I had an error making this commit it was saying:
    ```ImproperlyConfigured at /api/products/
    Could not resolve URL for hyperlinked relationship using view name "product-detail". You may have failed to include the related model in your API, or incorrectly configured the `lookup_field` attribute on this field.```
    
    that was only because I forgot to add the url field to the class
    ProductSerializer, just below the my_discount field.

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,9 +3,11 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields =[
+            'url',
             'pk',
             'title',
             'content',
@@ -13,6 +15,9 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+
+    def get_url(self, obj):
+        return f"/api/products/{obj.pk}"
 
     def get_my_discount(self,obj):
         if not hasattr(obj,'id'):
