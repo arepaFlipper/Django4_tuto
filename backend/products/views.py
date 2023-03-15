152cdb4b2cc69e5a5ca1ebf28a454b2672262398
cris <ctovar@abexuspr.com>
2023-03-15 05:26:08 -0500

    🎋 (3:30:30)use reverse to declare the url field in serializer
    To see the url routing to the detailed page, restore the
    ProductDetailAPIView as well.

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView 
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from rest_framework.permissions import  IsAdminUser
 from .models import Product
 from api.mixins import StaffEditorPermissionMixin
@@ -20,3 +20,8 @@ class ProductListCreateAPIView(ListCreateAPIView, StaffEditorPermissionMixin):
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
+class ProductDetailAPIView(StaffEditorPermissionMixin, RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
