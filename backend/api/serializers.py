ef41675aede66ecd768f4f440d977246f69a7eb2
cris <ctovar@abexuspr.com>
2023-03-16 18:49:05 -0500

    🐛 (4:22:44) make multiple experiments with serializer:
    The serializer will work with no trouble, even if we declare
    non-existing fields

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class UserProductInlineSerializer(serializers.Serializer):
     title = serializers.CharField(read_only=True)
 
-class UserPublicSerializer(serializers.Serializer):
+class UserPublicSerializer(serializers.ModelSerializer):
     username = serializers.CharField(read_only= True)
     this_is_not_real = serializers.CharField(read_only= True)
     id = serializers.IntegerField(read_only=True)
-    other_products = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username','this_is_not_real','id']
 
     def get_other_products(self,obj):
         print("""🕢   \x1b[1;34;40mserializers.py:9 obj:""") ## DELETEME
