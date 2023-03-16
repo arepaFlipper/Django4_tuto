ae5bc142c4c34c33b99f62c082d5f71e3e011a3d
cris <ctovar@abexuspr.com>
2023-03-16 18:55:41 -0500

    ⚜️ (4:24:28) restore serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,19 +5,8 @@ User = get_user_model()
 class UserProductInlineSerializer(serializers.Serializer):
     title = serializers.CharField(read_only=True)
 
-class UserPublicSerializer(serializers.ModelSerializer):
+class UserPublicSerializer(serializers.Serializer):
     username = serializers.CharField(read_only= True)
     this_is_not_real = serializers.CharField(read_only= True)
     id = serializers.IntegerField(read_only=True)
 
-    class Meta:
-        model = User
-        fields = ['username','this_is_not_real','id']
-
-    def get_other_products(self,obj):
-        print("""🕢   \x1b[1;34;40mserializers.py:9 obj:""") ## DELETEME
-        print(obj) ## DELETEME
-        print('\x1b[0m') ## DELETEME
-        user = obj
-        my_products_qs = user.product_set.all()[:5]
-        return UserProductInlineSerializer(my_products_qs,many=True, context=self.context).data
