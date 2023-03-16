522e226444f43b5584cff63fd5ea0365a98f2254
cris <ctovar@abexuspr.com>
2023-03-16 10:30:02 -0500

    🍴 (4:06:00) request details of product 27 were the owner is staff
    from "cfe" there is no authorization, but it is able to see it from "staff"

--- py_client/create.py
+++ py_client/create.py
@@ -5,7 +5,7 @@ endpoint = "http://localhost:7890/api/products/"
 token = get_token()
 headers = {'Authorization': f'Bearer {token}'}
 
-data = {"title":"Product ABc", "content":"if you can", "price": "32.99"}
+data = {"title":"Product new post", "content":"if you can", "price": "32.99"}
 get_response = requests.post (endpoint, json=data, headers=headers)
 print("status_code: "+str(get_response.status_code)) ## DELETEME
 print("""📗 \x1b[1;32;40mget_response.status_code:""") ## DELETEME
--- py_client/detail.py
+++ py_client/detail.py
@@ -1,8 +1,11 @@
 import requests
+from auth_token import get_token
 
-endpoint = "http://localhost:7890/api/products/1/"
+endpoint = "http://localhost:7890/api/products/27/"
+token = get_token()
+headers = {'Authorization': f'Bearer {token}'}
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, headers=headers)
 print("status_code: "+str(get_response.status_code)) ## DELETEME
 print("""📗 \x1b[1;36;40mget_response.status_code:""") ## DELETEME
 print(get_response.json())
