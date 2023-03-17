24571421f7d24264d22ecc9b7c9c8725934df12a
cris <ctovar@abexuspr.com>
2023-03-16 19:21:19 -0500

    🪢 (4:30:05) display next_url in a user that:
    User with 10 or more amount of products

--- py_client/auth_token.py
+++ py_client/auth_token.py
@@ -3,7 +3,7 @@ from getpass import getpass
 
 def get_token():
     # password = getpass() 
-    password = "random_pass" # this is bad I know don't look at me like that, I'm just too lazy to type password every single time
+    password = "qwerty" # this is bad I know don't look at me like that, I'm just too lazy to type password every single time
     auth_endpoint = "http://localhost:7890/api/auth/"
-    auth_response = requests.post(auth_endpoint, json={'username': 'staff', 'password': password})
+    auth_response = requests.post(auth_endpoint, json={'username': 'root', 'password': password})
     return auth_response.json()['token']
--- py_client/list.py
+++ py_client/list.py
@@ -14,3 +14,12 @@ print("status_code: "+str(get_response.status_code)) ## DELETEME
 print("""📗 \x1b[1;36;40mget_response.status_code:""") ## DELETEME
 print(get_response.json())
 print('\x1b[0m') ## DELETEME
+data = get_response.json()
+next_url = data['next']
+print("""▫️   \x1b[1;33;40mlist.py:20   next_url:""") ## DELETEME
+print(next_url) ## DELETEME
+print('\x1b[0m') ## DELETEME
+results = data['results']
+print("""🍺   \x1b[1;34;40mlist.py:20   results:""") ## DELETEME
+print(results) ## DELETEME
+print('\x1b[0m') ## DELETEME
