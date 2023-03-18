7b1ed680602fa796d04d18fe1ec910042ec2d5d6
cris <ctovar@abexuspr.com>
2023-03-18 07:52:40 -0500

    🪄 (5:34:53) update the first product

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -13,6 +13,7 @@ class ProductSerializer(serializers.ModelSerializer):
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field='pk')
     title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title ])
+    body = serializers.CharField(source='content')
 
     class Meta:
         model = Product
@@ -22,7 +23,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'edit_url',
             'pk',
             'title',
-            'content',
+            'body',
             'price',
             'sale_price',
         ]
--- py_client/update.py
+++ py_client/update.py
@@ -1,9 +1,12 @@
 import requests
+from auth_token import get_token
 
 endpoint = "http://localhost:7890/api/products/1/update/"
 
-data = {"title":"Hello world I love my dactyl manuform keyboards", "price":129.99}
-get_response = requests.put(endpoint, json=data)
+token = get_token()
+headers = {'Authorization': f'Bearer {token}'}
+data = {"title":"Another new product","body":"This is my new content", "price":3428930.23, "public": True}
+get_response = requests.put(endpoint, json=data, headers=headers)
 print("status_code: "+str(get_response.status_code)) ## DELETEME
 print("""📗 \x1b[1;36;40mget_response.status_code:""") ## DELETEME
 print(get_response.json())
