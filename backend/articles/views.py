4c6d4c34e9d8e9e6d6b741f7b4ad44a7252cbaa8
cris <ctovar@abexuspr.com>
2023-03-18 07:20:35 -0500

    ⌚ create a couple articles

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -1,8 +1,8 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView
 from .models import Article
 from .serializers import ArticleSerializer
 
-class ArticleListView(ListAPIView):
+class ArticleListView(ListCreateAPIView):
     queryset = Article.objects.public()
     serializer_class = ArticleSerializer
 
--- py_client/create.py
+++ py_client/create.py
@@ -1,11 +1,11 @@
 import requests
 from auth_token import get_token
 
-endpoint = "http://localhost:7890/api/products/"
+endpoint = "http://localhost:7890/api/articles/"
 token = get_token()
 headers = {'Authorization': f'Bearer {token}'}
 
-data = {"title":"Product new post", "content":"if you can", "price": "32.99"}
+data = {"title":"Blog Articles are Simple", "body":"This is a new blog article"}
 get_response = requests.post (endpoint, json=data, headers=headers)
 print("status_code: "+str(get_response.status_code)) ## DELETEME
 print("""📗 \x1b[1;32;40mget_response.status_code:""") ## DELETEME
