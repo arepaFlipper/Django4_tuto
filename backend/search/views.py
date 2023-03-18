f8b05fe0957c738aa3592be36db2fd5a14b74892
cris <ctovar@abexuspr.com>
2023-03-17 19:33:15 -0500

    ⚾ (5:14:06) add algolia client & update SearchListView
    📁 GenericAPIView allows methods declared in their classes

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,19 +1,13 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
+from . import client
 
-class SearchListView(ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-    def get_queryset(self, *args, **kwargs):
-        qs = super().get_queryset(*args,**kwargs)
-        q = self.request.GET.get('q')
-        results = Product.objects.none()
-        if q is not None:
-            user = None
-            if self.request.user.is_authenticated:
-                user = self.request.user
-            results = qs.search(q, user = user)
-        return results
-
+class SearchListView(GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        if not query:
+            return Response('', status=400)
+        results = client.perform_search(query)
+        return Response(results)
--- py_client/search.py
+++ py_client/search.py
@@ -1,7 +1,7 @@
 import requests
 from auth_token import get_token
 
-endpoint = "http://localhost:7890/api/search/?q=hello"
+endpoint = "http://localhost:7890/api/search/?q=my%20old%20friend"
 token = get_token()
 print("""❔   \x1b[1;33;40mlist.py:6    token:""") ## DELETEME
 print(token) ## DELETEME
@@ -11,15 +11,15 @@ headers = {
 }
 get_response = requests.get(endpoint, headers=headers)
 print("status_code: "+str(get_response.status_code)) ## DELETEME
-print("""📗 \x1b[1;36;40mget_response.status_code:""") ## DELETEME
-print(get_response.json())
-print('\x1b[0m') ## DELETEME
 data = get_response.json()
-next_url = data['next']
-print("""▫️   \x1b[1;33;40mlist.py:20   next_url:""") ## DELETEME
-print(next_url) ## DELETEME
+print("""📗 \x1b[1;36;40mget_response.status_code:""") ## DELETEME
+print(data)
 print('\x1b[0m') ## DELETEME
-results = data['results']
-print("""🍺   \x1b[1;34;40mlist.py:20   results:""") ## DELETEME
-print(results) ## DELETEME
-print('\x1b[0m') ## DELETEME
+# next_url = data['next']
+# print("""▫️   \x1b[1;33;40mlist.py:20   next_url:""") ## DELETEME
+# print(next_url) ## DELETEME
+# print('\x1b[0m') ## DELETEME
+# results = data['results']
+# print("""🍺   \x1b[1;34;40mlist.py:20   results:""") ## DELETEME
+# print(results) ## DELETEME
+# print('\x1b[0m') ## DELETEME
