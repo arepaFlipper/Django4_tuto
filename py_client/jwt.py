065bee0d29b8c5a66e31b34d63a845470eccdf2a
cris <ctovar@abexuspr.com>
2023-03-18 11:15:35 -0500

    🖨️ add jwt client

---
+++ py_client/jwt.py
@@ -0,0 +1,125 @@
+from dataclasses import dataclass
+import requests, pathlib, json
+from getpass import getpass
+
+@dataclass 
+class JWTClient:
+    access:str = None
+    refresh:str = None
+
+    header_type: str = "Bearer"
+    base_endpoint = "http://localhost:7890/api"
+
+    cred_path: pathlib.Path = pathlib.Path("creds.json")
+
+    def __post__init__(self):
+        if self.cred_path.exist():
+            try:
+                data = json.loads(self.cred_path.read_text())
+            except Exception as e:
+                print("""9️⃣   \x1b[1;31;40mjwt.py:21    Assuming creds has been tampered with""") ## DELETEME
+                print('\x1b[0m') ## DELETEME
+                data = None
+
+            if data is None:
+                self.clear_tokens()
+                self.perform_auth()
+            else:
+                self.access = data.get('access')
+                self.refresh = data.get('refresh')
+                token_verified = self.verify_token()
+                
+                if not token_verified:
+                    refreshed = self.perform_refresh()
+                    if not refreshed:
+                        print("""🕤   \x1b[1;31;40mjwt.py:36    invalid data, login again:""") ## DELETEME
+                        print('\x1b[0m') ## DELETEME
+                        self.clear_tokens()
+                        self.perform_auth()
+        else:
+            self.perform_auth()
+
+    def get_headers(self,header_type=None):
+        _type = header_type or self.header_type
+        token = self.access
+        if not token:
+            return {}
+        return {
+            "Authorization": f"{_type} {token}"
+        }
+
+    def perform_auth (self):
+        endpoint = f"{self.base_endpoint}/token/"
+        username = "staff"
+        password = "random_pass"
+        r = requests.post(endpoint, json={'username': username, 'password': password})
+        if r.status_code != 200:
+            raise Exception(f"Access not granted: {r.text}")
+
+        print('access granted')
+        self.write_creds(r.json())
+
+    def write_creds(self, data:dict):
+        if self.cred_path is not None:
+            self.access = data.get('access')
+            self.refresh = data.get('refresh')
+            if self.access and self.refresh:
+                self.cred_path.write_text(json.dumps(data))
+
+    def verify_token(self):
+        data = {
+            "token": f"{self.access}"
+        }
+        endpoint = f"{self.base_endpoint}/token/verify/"
+        r = requests.post(endpoint,json=data)
+        return r.status_code == 200
+
+    def clear_tokens(self):
+        self.access = None
+        self.refresh = None
+        if self.cred_path.exists():
+            self.cred_path.unlink()
+
+    def perform_refresh(self):
+        print("Refresing token.")
+        headers = self.get_headers()
+        data = { "refresh": f"{self.refresh}"}
+        endpoint = f"{self.base_endpoint}/token/refresh"
+        r= requests.post(endpoint, json=data, headers=headers)
+        if r.status_code != 200:
+            self.clear_tokens()
+            return False
+        refresh_data = r.json()
+        if not 'access' in refresh_data:
+            self.clear_tokens()
+            return False
+        stored_data = {
+            'access': refresh_data.get('access'),
+            'refresh': self.refresh
+        }
+        self.write_creds(stored_data)
+        return True
+
+    def list(self, endpoint=None, limit=3):
+        headers = self.get_headers()
+        if endpoint is None or self.base_endpoint not in str(endpoint):
+            endpoint = f"{self.base_endpoint}/products/?limit={limit}"
+        r = requests.get(endpoint, headers=headers)
+        if r.status_code !=200:
+            raise Exception(f"Request not complete {r.text}")
+        data = r.json()
+        return data
+
+if __name__ == "__main__":
+    client = JWTClient()
+    lookup_1_data = client.list(limit=5)
+    results = lookup_1_data.get('results')
+    next_url = lookup_1_data.get('next')
+    print("First lookup result length", len(results))
+    if next_url:
+        lookup_2_data = client.list(endpoint=next_url)
+        results += lookup_2_data.get('results')
+        print("""🖍️   \x1b[1;36;40mjwt.py:125   Second lookup result length:""") ## DELETEME
+        print(len(results)) ## DELETEME
+        print('\x1b[0m') ## DELETEME
+
